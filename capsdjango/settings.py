d2f6dcae0676b77d65ddac5be63865c3cb7d80a2
Jacinto José Franco <geekaia@gmail.com>
2018-01-09 00:18:53 -0300

    Data

--- capsdjango/settings.py
+++ capsdjango/settings.py
@@ -144,10 +144,10 @@ STATICFILES_DIRS = [
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+#
 
 
